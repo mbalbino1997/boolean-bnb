452c2f9cabc16d1dda5ddb9ea5def7e2643d736d
mbalbino1997 <rmassimo.balbino@gmail.com>
2025-02-16 20:13:46 +0100

    add

--- client/src/components/HouseList/HouseList.jsx
+++ client/src/components/HouseList/HouseList.jsx
@@ -7,10 +7,20 @@ import Loader from "../Loader/Loader"
 import style from "./HouseList.module.css"
 
 export default function HouseList() {
-    const { houses, setHouses, searchedCity, fetchHouses } = useContext(GlobalContext)
+    const { houses, setHouses, searchedCity, fetchHouses, setSearchedCity, filters, setFilters, setSearchParams } = useContext(GlobalContext)
 
     const location = useLocation();
 
+    function handleSearch(e) {
+        const { name, value } = e.target;
+        const newFilters = {
+            ...filters,
+            city: searchedCity
+        };
+        setFilters(newFilters);
+        setSearchParams(newFilters);
+        // fetchHouses()
+    }
     useEffect(() => {
 
         const searchParams = new URLSearchParams(location.search);
@@ -38,31 +48,43 @@ export default function HouseList() {
 
 
     return (<>
-        <div className={style.house_type_bar}>
-            <button>Appartamento</button>
-            <button>Casa indipendente</button>
-            <button>Villa</button>
-            <button>Villetta a schiera</button>
-            <button>Chalet</button>
-            <button>Baita</button>
-        </div>
         {!houses ? (<Loader />) : houses.length > 0 ?
             (<div className={style.main_wrapper}>
                 <div className={style.search_sidebar}>
-                    <input type="text" placeholder="Cerca per città..." />
-                    <button>search</button>
-                </div>
-                <div className="container">
-                    <div className="row d-flex flex-wrap row-gap-5 pb-5">
-                        {houses.map((house, i) => (
-                            <div key={i} className="col-lg-4 col-md-6 col-xs-12">
-                                <Link className="text-decoration-none text-dark" to={`/${house.title.replace(/ /g, '-')}`}>
-                                    <HouseCard content={house} />
-                                </Link>
-                            </div>
-                        ))}
+                    <div className={style.search}>
+
+                        <input type="text"
+                            placeholder="Cerca per città..."
+                            name="city"
+                            value={searchedCity}
+                            onChange={(e) => setSearchedCity(e.target.value)} />
+                        <button onClick={handleSearch}><img src="search.png" alt="" /></button>
                     </div>
                 </div>
+                <div className="flex-grow-1">
+                    <div className={style.house_type_bar}>
+                        <button>Appartamento</button>
+                        <button>Casa indipendente</button>
+                        <button>Villa</button>
+                        <button>Villetta a schiera</button>
+                        <button>Chalet</button>
+                        <button>Baita</button>
+                    </div>
+                    <div className="container">
+                        <div className="row d-flex flex-wrap row-gap-5 mt-5 pb-5">
+                            {houses.map((house, i) => (
+                                <div key={i} className="col-lg-4 col-md-6 col-xs-12">
+                                    <Link className="text-decoration-none text-dark" to={`/${house.title.replace(/ /g, '-')}`}>
+                                        <HouseCard content={house} />
+                                    </Link>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+
+                </div>
+
             </div>) :
             (<h1 className="text-center">La ricerca non ha prodotto risultati</h1>)
         }
